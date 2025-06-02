a40df22ace3b679e9860d4b539e05d7b7abef58b
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-06-02 18:29:22 +0530

    switch-case done

--- switch_case/switch.go
+++ switch_case/switch.go
@@ -1,6 +1,52 @@
 package main
 
-func main()  {
-	//simple switch
+import (
+	"fmt"
 	
+)
+
+func main() {
+	//simple switch
+	// i := 3
+	// switch i {
+	// case 1:
+	// 	fmt.Println("one")
+	// case 2:
+	// 	fmt.Println("two")
+	// default:
+	// 	fmt.Println("three")
+	// }
+
+
+
+
+	//multiple condition switch
+	// switch time.Now().Weekday(){
+	// case time.Saturday, time.Sunday:
+	// 	fmt.Println("its weekend")
+	// default:
+	// 	fmt.Println("its workday")
+	// }
+
+
+
+
+
+
+	//type switch
+	whoAmI := func (i interface{})  {
+		switch t := i.(type){
+		case int:
+			fmt.Println("its an integer")
+		case string:
+			fmt.Println("its a string")
+		case bool:
+			fmt.Println("its a boolean")
+		default:
+			fmt.Println("other", t)
+		}
+	}
+
+
+	whoAmI(3.56)
 }
