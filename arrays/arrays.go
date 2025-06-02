97727ef08ec5a1c590094c534bc097f4a2cf6e73
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-06-02 22:32:56 +0530

    arrays done

---
+++ arrays/arrays.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nums [3]int
+	// nums[0] = 1
+	// fmt.Println(nums[0])
+	// fmt.Println(nums)
+	// fmt.Println(len(nums))
+
+
+
+	// var vals [4]bool
+	// fmt.Println(vals)
+
+	// var str [2]string
+	// str[0] = "name"
+	// fmt.Println(str)
+
+	// //for empty values(int--> 0, bool--> faolse, string--> " ")
+
+
+	// nums := [3]int{1, 2, 5}
+	// fmt.Println(nums)
+
+
+
+
+	//2D arrays
+	nums := [2][2]int{{6, 7}, {9, 10}}
+	fmt.Println(nums)
+
+
+
+	//- fixed size, that is predictable
+	//- memory optimization
+	//- Content time access
+
+}
