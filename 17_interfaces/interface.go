ee6425401d1efe0e045d30a7c1aec0b91b640944
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-06-15 19:43:29 +0530

    interfaces updated

--- 17_interfaces/interface.go
+++ 17_interfaces/interface.go
@@ -3,23 +3,34 @@ package main
 import "fmt"
 
 type payment struct {
-
+	gateway razorpay
+	
 }
 
 func (p payment) makePayment(amount float32) {
-	razorpayPaymentGw := razorpay{}
-	razorpayPaymentGw.pay(amount)
+	// razorpayPaymentGw := razorpay{}
+	// razorpayPaymentGw.pay(amount)
+
+	// stripePaymentGw := stripe{}
+	// 	p.gateway.payUsingStripe(amount)
+	p.gateway.payUsingRazorpay(amount)
+
 }
 
-type razorpay struct {
+type razorpay struct {}
+
+type stripe struct{}
+
+func (s stripe) payUsingStripe(amount float32){
+	fmt.Println("making payment using stripe", amount)
 }
 
-func (r razorpay) pay(amount float32) {
+func (r razorpay) payUsingRazorpay(amount float32) {
 	//logic to make payment
 	fmt.Println("making payment using razorpay", amount)
 }
 
 func main() {
 	newPayment := payment{}
-	newPayment.makePayment(100)
+	newPayment.makePayment(50000)
 }
